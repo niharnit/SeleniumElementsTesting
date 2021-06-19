4f0aae43b280ef4cb32dfb04d734cf0b63c424fd
Nihar Ranjan Panda <nihar.nit@gmail.com>
2021-06-19 06:09:20 -0400

    comments added in Helper class for better documentation

--- src/test/java/com/automationpractice/utility/Helper.java
+++ src/test/java/com/automationpractice/utility/Helper.java
@@ -13,6 +13,8 @@ import org.openqa.selenium.io.FileHandler;
 
 public class Helper {
 
+	
+	//The below method is to create screenshot.png
 	public void captureScreenshots(WebDriver driver) {
 		
 		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
@@ -27,13 +29,16 @@ public class Helper {
 			System.out.println("Unable to take Screenshot" + e.getMessage());
 		}
 	}
-
+	
+	//The below method to create current date and time to add in screenshot.png 
 	public String getCurrentDateTime() {
 		 
 		DateFormat customFormat = new SimpleDateFormat("MM_dd_yyyy_HH_mm_ss");
 		Date currentDate = new Date();
 		return customFormat.format(currentDate);
 	}	
+	
+	
 	//The below method used to add wait time based on arugument in seconds
 	public static void delayInSeconds(int delayNumber) {
 		
